b39f7ed57a90627f1218552f9e4c0cbed4c4fb14
Alex Miles <alex.miles@powerverse.com>
2024-07-31 03:32:04 +0100

    Make value() helper compatible with Laravel 8+

--- src/helpers.php
+++ src/helpers.php
@@ -223,11 +223,11 @@ if ( ! function_exists('value'))
 	 * Return the default value of the given value.
 	 *
 	 * @param  mixed  $value
-* @return mixed
+	 * @return mixed
 	 */
-	function value($value)
+	function value($value, ...$args)
 	{
-		return $value instanceof Closure ? $value() : $value;
+		return $value instanceof Closure ? $value(...$args) : $value;
 	}
 }
 
